3aa9c34c7fe182e3c6962c5e3bbf56eb053f3f00
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-08-31 15:43:18 +1000

    add change from previous commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
-}
+  hideTime = true;
+  message;
+  messages: {text: string, time: string}[]
+  data: {message: string, time: string}
+    constructor() {}
+  
+    sendMessage(inputField){
+      console.log(inputField)
+    }
+  
+  }
+  
