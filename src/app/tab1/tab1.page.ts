54f1fd8cfe1870ecd32ec5526fa8dae8ab4a0bbb
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-08-31 17:56:18 +1000

    update page layout for aesthetics add basic socket io functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from './socket.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
   hideTime = true;
+  messageList: any[] = [];
+  ioConnection: any;
   message;
-  messages: {text: string, time: string}[]
-  data: {message: string, time: string}
-    constructor() {}
-  
-    sendMessage(inputField){
-      console.log(inputField)
+  messages: { text: string | number; time: string }[];
+  data: { message: string; time: string };
+  constructor(private socketService: SocketService) {}
+  sendMessage(text: any) {
+    if (text) {
+      this.socketService.sendMessage(text);
     }
-  
   }
-  
+  ngOnInit() {
+    this.initIoConnection();
+  }
+  private initIoConnection() {
+    this.socketService.initSocket();
+    this.ioConnection = this.socketService
+      .getMessage()
+      .subscribe((message: {message: string, time: Date}) => {
+        this.messageList.push({message: message.message, time: String(new Date(message.time).toTimeString().slice(0,8)), timeVisible: false});
+      });
+  }
+}
