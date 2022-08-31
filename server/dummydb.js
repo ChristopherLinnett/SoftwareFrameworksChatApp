0f32bd23ff6cb5b1fd29ec6e22961f3c34e5f850
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-08-31 21:11:18 +1000

    add dummydata for mocking up user database add server code for handling login

---
+++ server/dummydb.js
@@ -0,0 +1,7 @@
+module.exports = {
+    users: {1: {username: "superuser", email: "superuser@gmail.com", ID: "1", password: "superuser"},
+            2: {username: "groupuser", email: "groupuser@gmail.com", ID: "2", password: "groupuser"},
+            3: {username: "assistantuser", email: "assistantuser@gmail.com", ID: "3",password: "assistantuser"}},
+    superUsers: [{ID: 1}],
+    groupUsers: [{ID: 2}]
+}
--- server/server.js
+++ server/server.js
@@ -2,23 +2,38 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 var http = require('http').Server(app);
+const bodyparser = require('body-parser')
 const io = require('socket.io')(http, {
     cors: {
-        origin: 'http://localhost:8100',
+        origin: '*',
         methods: ["GET", "POST"],
     }
 });
 const socket = require ('./socket.js');
 const server = require('./listen.js')
+const dummyData = require('./dummydb.js');
+
 
 const PORT = 3000;
 
 
 app.use(cors());
+app.use(bodyParser.json());
 
 socket.connect(io,PORT);
 
 server.listen(http, PORT);
 
-
+app.post('/auth', (req,res)=>{
+    username = req.body.username
+    password = req.body.password
+    for (const [key, value] of Object.entries(dummyData.users)) {
+        if (username && username == value.username && password == value.password){
+        console.log("success")
+       return res.send({username: value.username, id: value.ID, email: value.email, loginSuccess: true});
+    }
+}
+    console.log("fail")
+    res.send({loginSuccess: false});
+})
 
