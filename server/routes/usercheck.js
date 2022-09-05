fc0b426f6b1b34c84a26bc1ea11b8338ff0acbc6
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-09-06 02:34:40 +1000

    admin page outputs role admin html conditional rendering based on state upgradeuser method added

--- server/routes/usercheck.js
+++ server/routes/usercheck.js
@@ -1,28 +1,53 @@
 module.exports = (app, dummyData) => {
-    app.post("/admin/usercheck", (req, res) => {
-  username = req.body.username;
-  if (!username.includes("@") && dummyData.users[username]) {
-    savedUser = dummyData.users[username];
+  app.post("/admin/usercheck", (req, res) => {
+    username = req.body.username;
+    if (!username.includes("@") && dummyData.users[username]) {
+      savedUser = dummyData.users[username];
+      if (dummyData["superUsers"].includes(savedUser.ID)) {
+        role = "superuser";
+      }
+      if (dummyData["groupUsers"].includes(savedUser.ID)) {
+        role = "groupuser";
+      }
+      if (!role) {
+        role = "user";
+      }
       return res.send({
         username: savedUser.username,
         id: savedUser.id,
         email: savedUser.email,
-        validUser: true,
-      });
-  }
-  processList = Object.keys(dummyData.users).map((username)=>{return dummyData.users[username]})
-  emailList = processList.map((userprofile)=>{return [userprofile.email, userprofile.username]})
-  for (let email of emailList) {
-    if (username == email[0]) {
-      savedUser = dummyData.users[email[1]];
-      return res.send({
-        username: savedUser.username,
-        id: savedUser.id,
-        email: savedUser.email,
+        role: role,
         validUser: true,
       });
     }
-  }
-  res.send({ validUser: false });
-});
-}
+    processList = Object.keys(dummyData.users).map((username) => {
+      return dummyData.users[username];
+    });
+    emailList = processList.map((userprofile) => {
+      return [userprofile.email, userprofile.username];
+    });
+    for (let email of emailList) {
+      if (username == email[0]) {
+        console.log('reached here')
+        savedUser = dummyData.users[email[1]];
+        if (dummyData["superUsers"].includes(savedUser.ID)) {
+          role = "superuser";
+        }
+        if (dummyData["groupUsers"].includes(savedUser.ID)) {
+          role = "groupuser";
+        }
+        if (!role) {
+          role = "user";
+        }
+        return res.send({
+          username: savedUser.username,
+          id: savedUser.ID,
+          email: savedUser.email,
+          role: role,
+          validUser: true,
+        });
+      }
+    }
+    res.send({ validUser: false });
+  });
+};
--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -7,61 +7,93 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-currentState = "unchecked";
-queryUser: {username: string, email: string, id: string};
-secondaryInput = "username"
-@ViewChild('userToCheck') input1;
-@ViewChild('input2') input2;
-  constructor(private httpClient: HttpClient) { }
+  usertypes = ['user', 'groupuser', 'superuser'];
+  currentState = 'unchecked';
+  queryUser: { username: string; email: string; id: string; role: string };
+  secondaryInput = 'username';
+  @ViewChild('userToCheck') input1;
+  @ViewChild('input2') input2;
+  @ViewChild('title') title;
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  resetform(){
-    this.currentState="unchecked"
-    this.queryUser = undefined
-    this.secondaryInput = "username"
-    this.input1.value = ''
-    this.input2.value = ''
+  resetform() {
+    this.currentState = 'unchecked';
+    this.queryUser = undefined;
+    this.secondaryInput = 'username';
+    this.input1.value = '';
+    this.input2.value = '';
   }
 
   checkUser(user): void {
-    this.httpClient.post<any>('http://localhost:3000/admin/usercheck', 
-    {username: user.toLowerCase()}).subscribe((res: {username: string, id: string, email: string, validUser: boolean}) => {
-      if (res.validUser) {
-          this.currentState = "deleteMode"
-          this.queryUser = {username: res.username, email: res.email, id: res.id}
-          this.input1.value=this.queryUser['username']
-          this.input2.value=this.queryUser['email']
-          sessionStorage.setItem('savedUser', JSON.stringify(res));
-        } else {
-        this.currentState = "createMode"
-        if (!user.includes("@")){
-          this.secondaryInput = "email"
+    this.httpClient
+      .post<any>('http://localhost:3000/admin/usercheck', {
+        username: user.toLowerCase(),
+      })
+      .subscribe(
+        (res: {
+          username: string;
+          id: string;
+          email: string;
+          role: string;
+          validUser: boolean;
+        }) => {
+          if (res.validUser) {
+            this.currentState = 'deleteMode';
+            this.queryUser = {
+              username: res.username,
+              email: res.email,
+              id: res.id,
+              role: res.role,
+            };
+            this.title.el.textContent =
+              this.queryUser['username'].toLocaleUpperCase();
+            this.input1.value = this.queryUser['email'];
+            this.input2.value = this.queryUser['role'];
+            sessionStorage.setItem('savedUser', JSON.stringify(res));
+          } else {
+            this.currentState = 'createMode';
+            if (!user.includes('@')) {
+              this.secondaryInput = 'email';
+            }
+          }
         }
-      }
-    });      
-}
-
-deleteUser(user){
-  this.httpClient.post<any>('http://localhost:3000/admin/deleteuser', 
-    {user: user}).subscribe((res: {success: Boolean}) => {
-      this.resetform()
-  })
-}
-
-createUser(input1,input2){
-  var newUser;
-  if (this.secondaryInput == 'email'){
-    newUser = {username: input1.toLowerCase(), email: input2.toLowerCase()}
+      );
   }
-    else {
-      newUser = {username: input2.toLowerCase(), email: input1.toLowerCase()}
+  async upgradeUser(direction) {
+    this.currentState='roleChange'
+    var currentLevel = this.usertypes.indexOf(this.input2.value);
+    console.log(currentLevel)
+    if ((direction == '+' && currentLevel < this.usertypes.length-1)) {
+      this.input2.value = this.usertypes[currentLevel + 1];
     }
-    this.httpClient.post<any>('http://localhost:3000/admin/newuser', 
-    newUser).subscribe((res: {success: Boolean}) => {
-      this.resetform()
-  })
-}
+    if ((direction == '-' && currentLevel > 0)) {
+      this.input2.value = this.usertypes[currentLevel - 1];
+    }
+    if (this.input2.value == this.queryUser.role){
+      this.currentState='deleteMode'
+    }
+  }
+  deleteUser(user): void {
+    this.httpClient
+      .post<any>('http://localhost:3000/admin/deleteuser', { user: user })
+      .subscribe((res: { success: Boolean }) => {
+        this.resetform();
+      });
+  }
 
+  createUser(input1, input2) {
+    var newUser;
+    if (this.secondaryInput == 'email') {
+      newUser = { username: input1.toLowerCase(), email: input2.toLowerCase() };
+    } else {
+      newUser = { username: input2.toLowerCase(), email: input1.toLowerCase() };
+    }
+    this.httpClient
+      .post<any>('http://localhost:3000/admin/newuser', newUser)
+      .subscribe((res: { success: Boolean }) => {
+        this.resetform();
+      });
+  }
 }
