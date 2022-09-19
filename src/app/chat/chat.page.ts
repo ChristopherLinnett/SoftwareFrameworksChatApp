b908ffa31d718e7766a393a71250728a30a99d76
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-09-20 00:20:49 +1000

    update capacitor core move socket service to shared services add http service for populating

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { SocketService } from './socket.service';
 import { IonContent } from '@ionic/angular';
 import { AuthService } from '../auth/auth.service';
+import { SocketService } from '../shared/services/socket.service';
 
 @Component({
   selector: 'app-chat',
